c3878cc57f1f0eca840f8799737a4752a9a562a6
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-08-04 04:45:25 +0700

    5.3-5-Step 2: Add score LiveData to the score_fragment.xml file

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -56,6 +56,10 @@ class ScoreFragment : Fragment() {
                 .get(ScoreViewModel::class.java)
         binding.scoreViewModel = viewModel
 
+        // Specify the fragment view as the lifecycle owner of the binding.
+// This is used so that the binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner
+
         // Add observer for score
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
